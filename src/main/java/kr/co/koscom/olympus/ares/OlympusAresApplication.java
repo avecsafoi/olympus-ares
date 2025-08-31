4d84644d78dc4659187b99fcd91b928bf3efdd41
KOSCOM <KOSCOM@host.docker.internal>
2025-08-31 18:52:50 +0900

    .

---
+++ src/main/java/kr/co/koscom/olympus/ares/OlympusAresApplication.java
@@ -0,0 +1,13 @@
+package kr.co.koscom.olympus.ares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympusAresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympusAresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/koscom/olympus/ares/OlympusAresApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.koscom.olympus.ares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympusAresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
